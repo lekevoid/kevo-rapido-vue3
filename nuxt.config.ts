e7fd96829f7799b0a26b97a547d1581540bad366
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2025-03-01 23:05:42 -0500

    Tweak PWA configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,30 @@ export default defineNuxtConfig({
 			styles: ["normal"],
 		},
 	},
+
+	pwa: {
+		manifest: {
+			name: "Kevo-Rapido",
+			short_name: "KevoRapido",
+			theme_color: "#008",
+			icons: [
+				{
+					src: "icons/android-chrome-192x192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+				{
+					src: "icons/android-chrome-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+				},
+				{
+					src: "icons/android-chrome-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+					purpose: "any maskable",
+				},
+			],
+		},
+	},
 });
